7de1585fb22153c3e0b675615f26da3349356809
OlenaVla <evlasenko409@gmail.com>
2022-02-02 19:12:56 +0200

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   let ind = 0; 
+   let resStr = '';
+   while (ind < expr.length)
+   {
+     let str = expr.slice(ind, ind + 10);
+     let i = str.indexOf('1');
+     ind += 10;
+     if (i === -1)
+     {
+        resStr +=' ';
+        continue;
+     }
+     let morse_code = '';
+     while (i < str.length)
+     {
+        morse_code += str.slice(i, i + 2) === '10' ? '.' : '-';
+        i += 2;
+     }
+     resStr += MORSE_TABLE[morse_code];
+    }
+   return resStr;
 }
 
 module.exports = {
